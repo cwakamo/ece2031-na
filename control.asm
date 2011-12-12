0a0851ef7a753c0b6f815804eb877015d36a01e2
Connor Wakamo <cwakamo@me.com>
2011-12-11 21:43:23 -0500

    Updated some formatting.

--- control.asm
+++ control.asm
@@ -1,17 +1,23 @@
+; control.asm (SCOMP assembly)
+; This code implements a program that controls an
+; AmigoBot for the purposes of wall-following.
+; Team NA
+; ECE 2031 L05
+; 12/07/2011
 			ORG 	&H000
 
 Safe_2:  	IN      XIO           	; Don't anything until key pressed again
         	AND     Key3Mask      	; Is the user pressing key 3?
         	JPOS    Safe_2         	; If not, wait
 
-MANUALDETECT:	IN SWITCHES
-				STORE CHECK
-				AND One
-				JZERO AUTODETECT
-				LOAD CHECK
-				AND Two
-				JPOS SETUP_R
-				JUMP SETUP_L
+MANUALDETECT: IN 	SWITCHES
+			STORE 	CHECK
+			AND 	One
+			JZERO 	AUTODETECT
+			LOAD 	CHECK
+			AND 	Two
+			JPOS 	SETUP_R
+			JUMP 	SETUP_L
         	
 AUTODETECT: LOAD	EnSonar0
 			OR		EnSonar5
@@ -28,24 +34,22 @@ AUTODETECT: LOAD	EnSonar0
 SETUP_R:	LOAD	One
 			OUT 	LEDS
 			LOAD	EnSonar3		;enable the front sonar
-			OR		EnSonar5	;enable the side sonar
-			OR		EnSonar4
+			OR		EnSonar5		;enable the side sonar
+			OR		EnSonar4		;enable the middle sonar
 			OUT		SONAREN			;write to the control register which sonars we want to enable
 			
-CHECK_FRONT_R: IN		DIST3
-			;AND		MAX
+CHECK_FRONT_R: IN	DIST3
 			OUT		SEVENSEG
 			SUB		MINFRONTTHRESH 	;subtract the front threshold so we can figure out if we're below it.
-			JPOS	CHECK_CRASH_R		;it's positive, so we're not too close in the front
-			JUMP	INSIDE_TURN_R		;we're within the front threshold, so we need to make an inside turn
+			JPOS	CHECK_CRASH_R	;it's positive, so we're not too close in the front
+			JUMP	INSIDE_TURN_R	;we're within the front threshold, so we need to make an inside turn
 
-CHECK_CRASH_R: IN DIST4
-			SUB MINCRASHTHRESH
-			JPOS CHECK_SIDE_R
-			JUMP INSIDE_TURN_R
+CHECK_CRASH_R: IN 	DIST4
+			SUB 	MINCRASHTHRESH
+			JPOS	CHECK_SIDE_R
+			JUMP 	INSIDE_TURN_R
 
-CHECK_SIDE_R:	IN		DIST5
-			;AND		MAX
+CHECK_SIDE_R: IN	DIST5
 			OUT		LCD
 			SUB		NOSIDETHRESH	;subtract the threshold at which we think we're no longer next to a wall
 			JPOS	OUTSIDE_TURN_R	;since the value is positive after subtracting the no-wall threshold, we need to make an outside turn
@@ -54,73 +58,70 @@ CHECK_SIDE_R:	IN		DIST5
 			JNEG	MOVE_AWAY_R		;since it's negative, we're below the threshold so we need to move away from the wall
 			ADD		MINSIDETHRESH	;get back to the value that we initially had
 			SUB		MAXSIDETHRESH	;subtract the max side threshold so we can figure out if we're above it.
-			JPOS	MOVE_TOWARD_R		;since it's positive, we're above the threshold so we need to move toward
+			JPOS	MOVE_TOWARD_R	;since it's positive, we're above the threshold so we need to move toward
 
 			JUMP	MOVE_FORWARD_R	;we're within the thresholds, so we can just move forward
 
 INSIDE_TURN_R: LOAD One
-			OUT LEDS
+			OUT 	LEDS
 			LOAD	QSPEED
 			OUT		LVELCMD
 			LOAD	HALFSPEED
 			OUT		RVELCMD			;here we handle the mechanics necessary to make the inside turn
-			JUMP	CHECK_FRONT_R		;after we make changes to complete the turn, we go back and check the front again
+			JUMP	CHECK_FRONT_R	;after we make changes to complete the turn, we go back and check the front again
 
 OUTSIDE_TURN_R: LOAD Two
-			OUT LEDS
+			OUT 	LEDS
 			LOAD	OSPEED
 			OUT		RVELCMD
 			LOAD	HALFSPEED
 			OUT		LVELCMD
 			JUMP	CHECK_FRONT_R		
 
-MOVE_AWAY_R:	LOAD Three
-			OUT LEDS				;here we handle the mechanics necessary to move away from the wall
+MOVE_AWAY_R: LOAD 	Three
+			OUT 	LEDS			;here we handle the mechanics necessary to move away from the wall
 			LOAD	HALFSPEED
 			OUT     RVELCMD
 			LOAD	ASPEED
 			OUT     LVELCMD
-			JUMP	CHECK_FRONT_R		;after we make adjustments, we go back to check the front again
+			JUMP	CHECK_FRONT_R	;after we make adjustments, we go back to check the front again
 
 MOVE_TOWARD_R: LOAD Four		
-			OUT LEDS				;here we handle the mechanics necessary to move back toward the wall
+			OUT 	LEDS			;here we handle the mechanics necessary to move back toward the wall
 			LOAD	ASPEED
 			OUT     RVELCMD
 			LOAD	HALFSPEED
 			OUT     LVELCMD
-			JUMP	CHECK_FRONT_R		;after we make adjustments, we go back to check the front again
+			JUMP	CHECK_FRONT_R	;after we make adjustments, we go back to check the front again
 
-MOVE_FORWARD_R: 
-			LOAD Five
-			OUT LEDS
+MOVE_FORWARD_R: LOAD Five
+			OUT 	LEDS
 			LOAD	HALFSPEED
 			OUT     RVELCMD
 			OUT     LVELCMD			;here we handle the mechanics necessary to maintain a forward path
-			JUMP	CHECK_FRONT_R		;after we make adjustments, we go back to check the front again			
+			JUMP	CHECK_FRONT_R	;after we make adjustments, we go back to check the front again			
 
 ; FOLLOW LEFT
 			
 SETUP_L:	LOAD	Two
 			OUT		LEDS
 			LOAD	EnSonar0		;enable the side sonar
-			OR		EnSonar1	;enable the crash sonar
-			OR		EnSonar2	;enable the front sonar
+			OR		EnSonar1		;enable the crash sonar
+			OR		EnSonar2		;enable the front sonar
 			OUT		SONAREN			;write to the control register which sonars we want to enable
 			
-CHECK_FRONT_L: IN		DIST2
-			;AND		MAX
+CHECK_FRONT_L: IN	DIST2
 			OUT		SEVENSEG
 			SUB		MINFRONTTHRESH 	;subtract the front threshold so we can figure out if we're below it.
-			JPOS	CHECK_CRASH_L		;it's positive, so we're not too close in the front
-			JUMP	INSIDE_TURN_L		;we're within the front threshold, so we need to make an inside turn
+			JPOS	CHECK_CRASH_L	;it's positive, so we're not too close in the front
+			JUMP	INSIDE_TURN_L	;we're within the front threshold, so we need to make an inside turn
 
-CHECK_CRASH_L: IN DIST1
-			SUB MINCRASHTHRESH
-			JPOS CHECK_SIDE_L
-			JUMP INSIDE_TURN_L
+CHECK_CRASH_L: IN	DIST1
+			SUB 	MINCRASHTHRESH
+			JPOS	CHECK_SIDE_L
+			JUMP 	INSIDE_TURN_L
 
-CHECK_SIDE_L:	IN		DIST0
-			;AND		MAX
+CHECK_SIDE_L: IN	DIST0
 			OUT		LCD
 			SUB		NOSIDETHRESH	;subtract the threshold at which we think we're no longer next to a wall
 			JPOS	OUTSIDE_TURN_L	;since the value is positive after subtracting the no-wall threshold, we need to make an outside turn
@@ -129,49 +130,48 @@ CHECK_SIDE_L:	IN		DIST0
 			JNEG	MOVE_AWAY_L		;since it's negative, we're below the threshold so we need to move away from the wall
 			ADD		MINSIDETHRESH	;get back to the value that we initially had
 			SUB		MAXSIDETHRESH	;subtract the max side threshold so we can figure out if we're above it.
-			JPOS	MOVE_TOWARD_L		;since it's positive, we're above the threshold so we need to move toward
+			JPOS	MOVE_TOWARD_L	;since it's positive, we're above the threshold so we need to move toward
 
 			JUMP	MOVE_FORWARD_L	;we're within the thresholds, so we can just move forward
 
 INSIDE_TURN_L: LOAD One
-			OUT LEDS
+			OUT 	LEDS
 			LOAD	QSPEED
 			OUT		RVELCMD
 			LOAD	HALFSPEED
 			OUT		LVELCMD			;here we handle the mechanics necessary to make the inside turn
-			JUMP	CHECK_FRONT_L		;after we make changes to complete the turn, we go back and check the front again
+			JUMP	CHECK_FRONT_L	;after we make changes to complete the turn, we go back and check the front again
 
 OUTSIDE_TURN_L: LOAD Two
-			OUT LEDS
+			OUT 	LEDS
 			LOAD	OSPEED
 			OUT		LVELCMD
 			LOAD	HALFSPEED
 			OUT		RVELCMD
 			JUMP	CHECK_FRONT_L		
 
-MOVE_AWAY_L:	LOAD Three
-			OUT LEDS				;here we handle the mechanics necessary to move away from the wall
+MOVE_AWAY_L: LOAD 	Three
+			OUT 	LEDS			;here we handle the mechanics necessary to move away from the wall
 			LOAD	HALFSPEED
 			OUT     LVELCMD
 			LOAD	ASPEED
 			OUT     RVELCMD
-			JUMP	CHECK_FRONT_L		;after we make adjustments, we go back to check the front again
+			JUMP	CHECK_FRONT_L	;after we make adjustments, we go back to check the front again
 
 MOVE_TOWARD_L: LOAD Four		
-			OUT LEDS				;here we handle the mechanics necessary to move back toward the wall
+			OUT 	LEDS			;here we handle the mechanics necessary to move back toward the wall
 			LOAD	ASPEED
 			OUT     LVELCMD
 			LOAD	HALFSPEED
 			OUT     RVELCMD
-			JUMP	CHECK_FRONT_L		;after we make adjustments, we go back to check the front again
+			JUMP	CHECK_FRONT_L	;after we make adjustments, we go back to check the front again
 
-MOVE_FORWARD_L: 
-			LOAD Five
-			OUT LEDS
+MOVE_FORWARD_L: LOAD Five
+			OUT 	LEDS
 			LOAD	HALFSPEED
 			OUT     LVELCMD
 			OUT     RVELCMD			;here we handle the mechanics necessary to maintain a forward path
-			JUMP	CHECK_FRONT_L		;after we make adjustments, we go back to check the front again	
+			JUMP	CHECK_FRONT_L	;after we make adjustments, we go back to check the front again	
 			
 Zero:        DW    0
 One:         DW    1
